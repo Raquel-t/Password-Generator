513b1721045048741bab3bf02bfe222976da4128
Mona Lisa <130006996+Raquel-t@users.noreply.github.com>
2023-05-06 14:17:28 -0500

    additional changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
 var passwordLength = "";
-var specialCharacters = "`~!@#$%^&*()_+-=[]\{}|;':\",./<>?";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "0123456789";
-var pass = "";
-
+var specialCharacters = "`, ~, !, @, #, $, %, ^, &, *, (, ), _, +, -, =, [, ], \, {, }, |, \, <, >, ?";
+var lowerCase = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+var upperCase = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var password = "";
 optionPass = [];
 
 
@@ -16,26 +15,30 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var Password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
-  passwordText.value = Password;
+  passwordText.value = password;
 } 
 
 // function to prompt user for password options. 
-// received assistance from tutor with w3schools
-// Got function line 28 from instructor. 
-function getPasswordOptions(){
-  passwordLength = prompt("How many characters will your password be?");
- 
+// received assistance from tutor with concat and promt on w3schools
+// Got function line 28 from instructor.
+// removed else/concat and got assistance from student Emily, however code still did not generate. 
+function getPasswordOptions() {
+  optionPass = [];
+  
+  passwordLength = prompt("Enter Password between 8 and 128 characters.");
+   
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Password length has to be 8 - 128 digits. Please try again.")
-    return false;
+    alert("Password length has to be 8 - 128 digits. Please try again.");
+    
+    return null;
   }
-
+  // removed (else /concat and got assistance from student Emily, however code still did not generate.
   if(confirm("Would you like special Characters?")) {
     optionPass = optionPass.concat(specialCharacters);
-  } else ()
+  }
   
   if(confirm("Would you like lowercase letters?")) {
     optionPass = optionPass.concat(lowerCase);
@@ -46,39 +49,44 @@ function getPasswordOptions(){
   if(confirm("Would you like numbers in your password?")) {
     optionPass = optionPass.concat(numeric);
   }
-  return true;
+   
+   if(optionPass.length === 0) {
+    alert("Pick at least one option!");
+    return "Try again!";
+  }
   
+  else {
+    return optionPass;
+  }
+    
 }
+
 
 // function for getting a random element from an array
 // Got function line 57 from instructor, however kept getting undefined due to the (arr) added so I took it out. 
-// referenced from https://forum.freecodecamp.org/t/im-getting-really-discouraged-with-loops/535211
 function getRandom(){
   var password = "";
-  var arr = "`~!@#$%^&*()_+-=[]\{}|;':\,./<>?abcdefghijklmnopqrstuvwxyz23456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
+  passwordLength = "";
+  optionPass = [];
+ 
   for(var i = 0; i < passwordLength; i++) {
-  var index = Math.floor(Math.random() * arr.length);
-  password = password + arr[index];
+    var index = Math.floor(Math.random() * optionPass.length);
+    password += optionPass[index];
   }
-  return password
+  
+  return password;
 
 }
 
 // Function to generate password with user imput
 // Got function line 71 from instructor. 
+// 
 function generatePassword() {
   getPasswordOptions();
-  var pass = getRandom();
-  // if () {
+  var password = getRandom();
+  passwordLength = "";
+    
+  return password;
 
-  // }
-  passwordLength = ""; 
-  return pass
 }
 
-// the amount of characters will require a for loop within the function array. 
-// Got var line 79 from instructor
-// var generatePassword = function() {
-
-// }
